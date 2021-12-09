97c2fadb16e513476552e11bc4e945167a3cc5e4
brr3 <bruno.rees.1968@student.uu.se>
2021-12-09 12:09:44 +0100

    Env vars should now be set before running tests

--- tests/replication.cpp
+++ tests/replication.cpp
@@ -41,14 +41,6 @@ constexpr unsigned j_const = 2124481224;
 /** @brief A random double constant */
 constexpr double d_const = 1.0 / 3.0 * 3.14159;
 
-void _set_replication_policy(int id) {
-	size_t repl_policy = argo::env::replication_policy();
-	if (!repl_policy || repl_policy == !id) {
-		setenv("ARGO_REPLICATION_POLICY", std::to_string(id).c_str(), 1);
-		argo::env::init();
-	}
-}
-
 /**
  * @brief Class for the gtests fixture tests. Will reset the allocators to a clean state for every test
  */
@@ -67,12 +59,10 @@ protected:
  * @brief Simple test that a replicated char can be fetched by its host node using complete 
  * replication
  */
-TEST_F(replicationTest, completeReplicationLocal) {
-	if (argo_number_of_nodes() == 1) {
+TEST_F(replicationTest, localCharCR) {
+	if (argo_number_of_nodes() == 1 || argo::env::replication_policy() != 0) {
 		return;
 	}
-
-	_set_replication_policy(0);
 
 	char* val = argo::conew_<char>(c_const);
 
@@ -91,12 +81,10 @@ TEST_F(replicationTest, completeReplicationLocal) {
 /**
  * @brief Test that a replicated char can be fetched by remote nodes using complete replication
  */
-TEST_F(replicationTest, completeReplicationRemote) {
-	if (argo_number_of_nodes() == 1) {
+TEST_F(replicationTest, remoteCharCR) {
+	if (argo_number_of_nodes() == 1 || argo::env::replication_policy() != 0) {
 		return;
 	}
-
-	_set_replication_policy(0);
 
 	char* val = argo::conew_<char>(c_const);
 
@@ -116,12 +104,10 @@ TEST_F(replicationTest, completeReplicationRemote) {
  * @brief Test that a replicated array can be fetched locally and remotely using complete 
  * replication
  */
-TEST_F(replicationTest, completeReplicationArray) {
-	if (argo_number_of_nodes() == 1) {
+TEST_F(replicationTest, arrayCR) {
+	if (argo_number_of_nodes() == 1 || argo::env::replication_policy() != 0) {
 		return;
 	}
-
-	_set_replication_policy(0);
 
 	const std::size_t array_size = 10;
 	int* array = argo::conew_array<int>(array_size);
@@ -150,24 +136,12 @@ TEST_F(replicationTest, completeReplicationArray) {
 }
 
 /**
- * @brief Test that the replication policy can be changed at runtime
+ * @brief Test that the system can recover from a node going down using complete replication
  */
-TEST_F(replicationTest, replicationPolicyChange) {
-	
-	_set_replication_policy(1);
-	ASSERT_EQ(argo::env::replication_policy(), (std::size_t) 1);
-
-}
-
-/**
- * @brief Test that a char can be rebuilt using erasure coding
- */
-TEST_F(replicationTest, erasureCodingChar) {
-	if (argo_number_of_nodes() == 1) {
+TEST_F(replicationTest, nodeKillRebuildCR) {
+	if (argo_number_of_nodes() == 1 || argo::env::replication_policy() != 0) {
 		return;
 	}
-
-	_set_replication_policy(1);
 
 	char* val = argo::conew_<char>(c_const);
 
@@ -176,81 +150,17 @@ TEST_F(replicationTest, erasureCodingChar) {
 	}
 	argo::barrier();
 
-	char receiver = 'z';
-	argo::backend::get_repl_data(val, (void *)&receiver, 1);
-	ASSERT_EQ(*val, receiver);
+	char copy = *val;
+
+	// kill_node(argo_get_homenode(val));
+	// OR:
+	// update_alteration_table(argo_get_homenode(val));
+	// Note: The killed node will still run all the code here since it doesn't actually crash
+
+	if (argo_get_homenode(val) == argo::node_id()) {
+		ASSERT_EQ(copy, *val); // val should point to the replicated node now
+	}
 }
-
-// /**
-//  * @brief Test that the system can recover from a node going down using complete replication
-//  */
-// TEST_F(replicationTest, nodeKillRebuildCR) {
-// 	if (argo_number_of_nodes() == 1) {
-// 		return;
-// 	}
-
-// 	_set_replication_policy(0);
-
-// 	char* val = argo::conew_<char>(c_const);
-
-// 	if (argo::node_id() == 0) {
-// 		*val += 1;
-// 	}
-// 	argo::barrier();
-
-// 	char copy = *val;
-
-// 	// kill_node(argo_get_homenode(val));
-// 	// OR:
-// 	// update_alteration_table(argo_get_homenode(val));
-// 	// Note: The killed node will still run all the code here since it doesn't actually crash
-
-// 	if (argo_get_homenode(val) == argo::node_id()) {
-// 		ASSERT_EQ(copy, *val); // val should point to the replicated node now
-// 	}
-// }
-
-// /**
-//  * @brief Test that the system can recover from a node going down using erasure coding
-//  */
-// TEST_F(replicationTest, nodeKillRebuildEC) {
-// 	if (argo_number_of_nodes() == 1) {
-// 		return;
-// 	}
-
-// 	_set_replication_policy(0);
-
-// 	char* val = argo::conew_<char>(c_const);
-
-// 	if (argo::node_id() == 0) {
-// 		*val += 1;
-// 	}
-// 	argo::barrier();
-
-// 	char copy = *val;
-
-// 	// kill_node(argo_get_homenode(val));
-// 	// OR:
-// 	// update_alteration_table(argo_get_homenode(val));
-// 	// Note: The killed node will still run all the code here since it doesn't actually crash
-
-// 	if (argo_get_homenode(val) == argo::node_id()) {
-// 		ASSERT_EQ(copy, *val); // val should point to the replicated node now
-// 	}
-// }
-
-// /**
-//  * @brief Test that the system can recover from a node going down
-//  */
-// TEST_F(replicationTest, latencyCompleteReplication) {
-// 	if (argo_number_of_nodes() == 1) {
-// 		return;
-// 	}
-
-// 	_set_replication_policy(0);
-
-	
-// }
 
 /**
  * @brief The main function that runs the tests
