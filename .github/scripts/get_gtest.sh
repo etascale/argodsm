29a7d2140bc5edea4ee4a2e6f0c93ce6fb52022e
Kostis Sagonas <kostis@it.uu.se>
2022-02-02 23:10:21 +0100

    Newest googletest is now 1.11.0

--- .github/scripts/get_gtest.sh
+++ .github/scripts/get_gtest.sh
@@ -1,7 +1,7 @@
 #! /usr/bin/env bash
 
 cd tests
-wget https://github.com/google/googletest/archive/release-1.10.0.zip
-unzip release-1.10.0.zip
-mv googletest-release-1.10.0 googletest
+wget https://github.com/google/googletest/archive/release-1.11.0.zip
+unzip release-1.11.0.zip
+mv googletest-release-1.11.0 googletest
 cd ..
