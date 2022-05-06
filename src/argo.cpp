etascale/argodsm

commit 2288c8978fdb38d2e6ef5126cb3aa0347453380f

Completely clean state for each test group (#83)
This PR changes the behavior of argo::reset to, in addition to resetting
the allocators, also reset Argo's coherence state.
argo::reset is now called ahead of running each test to ensure that each
test executes from a clean state. This helps us expose bugs previously
hidden due to the dirty state of the different directories in the backend.

src/argo.cpp
@@ -8,9 +8,10 @@
 
 #include "allocators/collective_allocator.hpp"
 #include "allocators/dynamic_allocator.hpp"
+#include "backend/backend.hpp"
+#include "data_distribution/data_distribution.hpp"
 #include "env/env.hpp"
 #include "virtual_memory/virtual_memory.hpp"
-#include "data_distribution/data_distribution.hpp"
 
 namespace vm = argo::virtual_memory;
 namespace mem = argo::mempools;
@@ -56,11 +57,31 @@ namespace argo {
 		/* note: the backend must currently initialize before the mempool can be set */
 		backend::init(requested_argo_size, requested_cache_size);
 		default_global_mempool = new mp();
-		argo_reset();
+		reset();
 	}
 
 	void finalize() {
 		delete default_global_mempool;
+	}
+
+	static void reset_allocators() {
+		default_global_mempool->reset();
+		using namespace alloc;
+		using namespace mem;
+		collective_prepool = dynamic_memory_pool<global_allocator, NODE_ZERO_ONLY>(&default_global_allocator);
+		dynamic_prepool = dynamic_memory_pool<global_allocator, ALWAYS>(&default_global_allocator);
+		default_global_allocator = global_allocator<char>();
+		default_dynamic_allocator = default_dynamic_allocator_t();
+		default_collective_allocator = collective_allocator();
+		default_global_allocator.set_mempool(default_global_mempool);
+		default_dynamic_allocator.set_mempool(&dynamic_prepool);
+		default_collective_allocator.set_mempool(&collective_prepool);
+	}
+
+	void reset() {
+		argo::backend::reset_coherence();
+		reset_allocators();
+		backend::barrier();
 	}
 
 	int node_id() {
@@ -86,17 +107,7 @@ extern "C" {
 	}
 
 	void argo_reset() {
-		default_global_mempool->reset();
-		using namespace alloc;
-		using namespace mem;
-		collective_prepool = dynamic_memory_pool<global_allocator, NODE_ZERO_ONLY>(&default_global_allocator);
-		dynamic_prepool = dynamic_memory_pool<global_allocator, ALWAYS>(&default_global_allocator);
-		default_global_allocator = global_allocator<char>();
-		default_dynamic_allocator = default_dynamic_allocator_t();
-		default_collective_allocator = collective_allocator();
-		default_global_allocator.set_mempool(default_global_mempool);
-		default_dynamic_allocator.set_mempool(&dynamic_prepool);
-		default_collective_allocator.set_mempool(&collective_prepool);
+		argo::reset();
 	}
 
 	int argo_node_id() {

src/argo.hpp
@@ -60,6 +60,12 @@ namespace argo {
 	void finalize();
 
 	/**
+	 * @brief reset ArgoDSM system
+	 * @warning this is mainly intended for testing, not for public use
+	 */
+	void reset();
+
+	/**
 	 * @brief A unique ArgoDSM node identifier. Counting starts from 0.
 	 * @return The node ID
 	 */

src/backend/backend.hpp
@@ -96,6 +96,12 @@ namespace argo {
 		 */
 		void finalize();
 
+		/**
+		 * @brief Resets current ArgoDSM coherence
+		 * @note Collective function which should be called only by one thread per node
+		 */
+		void reset_coherence();
+
 		/** @brief import global_ptr */
 		using argo::data_distribution::global_ptr;
 

src/backend/mpi/mpi.cpp
@@ -191,6 +191,10 @@ namespace argo {
 			argo_finalize();
 		}
 
+		void reset_coherence() {
+			argo_reset_coherence();
+		}
+
 		void barrier(std::size_t tc) {
 			swdsm_argo_barrier(tc);
 		}

src/backend/mpi/swdsm.cpp
@@ -902,7 +902,7 @@ void argo_initialize(std::size_t argo_size, std::size_t cache_size){
 		cacheControl[i].dirty = CLEAN;
 	}
 
-	argo_reset_coherence(1);
+	argo_reset_coherence();
 }
 
 void argo_finalize(){
@@ -1009,10 +1009,16 @@ void swdsm_argo_barrier(int n){ //BARRIER
 	}
 }
 
-void argo_reset_coherence(int n){
+void argo_reset_coherence(){
 	stats.writebacks = 0;
 	stats.stores = 0;
+
 	memset(touchedcache, 0, cachesize);
+	for(std::size_t i = 0; i < cachesize; i++){
+		cacheControl[i].tag = GLOBAL_NULL;
+		cacheControl[i].state = INVALID;
+		cacheControl[i].dirty = CLEAN;
+	}
 
 	sem_wait(&ibsem);
 	MPI_Win_lock(MPI_LOCK_EXCLUSIVE, workrank, 0, sharerWindow);
@@ -1038,10 +1044,11 @@ void argo_reset_coherence(int n){
 		}
 		MPI_Win_unlock(workrank, offsets_tbl_window);
 	}
+
 	sem_post(&ibsem);
-	swdsm_argo_barrier(n);
+	swdsm_argo_barrier(1);
 	mprotect(startAddr,size_of_all,PROT_NONE);
-	swdsm_argo_barrier(n);
+	swdsm_argo_barrier(1);
 	clearStatistics();
 }
 

src/backend/mpi/swdsm.h
@@ -198,10 +198,10 @@ void clearStatistics();
 void printStatistics();
 
 /**
- * @brief Resets current coherence. Collective function called by all threads on all nodes.
- * @param n number of threads currently spawned on each node. 
+ * @brief Resets current ArgoDSM coherence
+ * @note Collective function which should be called only by one thread per node
  */
-void argo_reset_coherence(int n);
+void argo_reset_coherence();
 
 /**
  * @brief Gives the ArgoDSM node id for the local process

src/backend/singlenode/singlenode.cpp
@@ -128,6 +128,26 @@ namespace argo {
 		void finalize() {
 		}
 
+		void reset_coherence() {
+			using namespace data_distribution;
+			/** @note first-touch needs a directory for fetching
+			 *        the homenode and offset for an address */
+			if (is_first_touch_policy()) {
+				/* calculate the directory size and allocate memory */
+				std::size_t owners_dir_size = 3*(memory_size/4096UL);
+				std::size_t owners_dir_size_bytes = owners_dir_size*sizeof(std::size_t);
+				owners_dir_size_bytes = (1 + ((owners_dir_size_bytes-1) / 4096UL))*4096UL;
+				global_owners_dir = static_cast<std::uintptr_t*>(vm::allocate_mappable(4096UL, owners_dir_size_bytes));
+				/* hardcode first-touch directory values so
+				   that we perform only load operations */
+				for(std::size_t j = 0; j < owners_dir_size; j += 3) {
+					global_owners_dir[j] = 0;
+					global_owners_dir[j+1] = j/3 * 4096UL;
+					global_owners_dir[j+2] = 0;
+				}
+			}
+		}
+
 		void barrier(std::size_t threadcount) {
 			/* initially: flag = false */
 			std::unique_lock<std::mutex> barrier_lock(barrier_mutex);

tests/allocators.cpp
@@ -32,8 +32,7 @@ class AllocatorTest : public testing::Test {
 
 	protected:
 		AllocatorTest() {
-			argo_reset();
-			argo::barrier();
+			argo::reset();
 		}
 
 		~AllocatorTest() {

commit 756ebfac81eec4422811de1eaab1affeec07858e

Fix doxygen warning of #83 (#89)
This commit fixes a doxygen warning caused by #83.

src/argo.cpp
@@ -64,6 +64,10 @@ namespace argo {
 		delete default_global_mempool;
 	}
 
+	/**
+	 * @brief reset ArgoDSM allocators
+	 * @warning this is mainly intended for testing, not for public use
+	 */
 	static void reset_allocators() {
 		default_global_mempool->reset();
 		using namespace alloc;