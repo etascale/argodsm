7627678d59ffcc90ec56e9ae59ff653b246536d0
Ioannis Anevlavis <ioannis.anevlavis@etascale.com>
2022-06-17 15:21:26 +0300

    Code style changes based on cpplint's feedback
    * Additionally, this commit keeps the number of lines between the
    test class and the first test, in between the individual tests, and
    between the last test and `main` consistent across the test files.
    
    That is:
    * 2 newlines between the test class and the first test.
    * 1 newline in between the individual tests.
    * 2 newlines between the last test and `main`.

--- tests/lock.cpp
+++ tests/lock.cpp
@@ -4,22 +4,24 @@
  * @copyright Eta Scale AB. Licensed under the Eta Scale Open Source License. See the LICENSE file for details.
  */
 
+// C headers
+#include <limits.h>
+#include <unistd.h>
+// C++ headers
 #include <iostream>
 #include <thread>
-#include <unistd.h>
-
+// ArgoDSM headers
 #include "argo.hpp"
-#include "allocators/generic_allocator.hpp"
 #include "allocators/collective_allocator.hpp"
+#include "allocators/generic_allocator.hpp"
 #include "allocators/null_lock.hpp"
 #include "backend/backend.hpp"
-#include "synchronization/global_tas_lock.hpp"
+#include "data_distribution/global_ptr.hpp"
 #include "synchronization/cohort_lock.hpp"
+#include "synchronization/global_tas_lock.hpp"
 #include "synchronization/intranode/mcs_lock.hpp"
 #include "synchronization/intranode/ticket_lock.hpp"
-#include "data_distribution/global_ptr.hpp"
-
-#include <limits.h>
+// GoogleTest headers
 #include "gtest/gtest.h"
 
 /** @brief ArgoDSM memory size */
@@ -36,50 +38,50 @@ constexpr int iter = 10000;
  * @brief Class for the gtests fixture tests. Will reset the allocators to a clean state for every test
  */
 class LockTest : public testing::Test {
+	protected:
+		/** @brief local ticket lock for testing */
+		argo::locallock::ticket_lock *ticket_lock;
+		/** @brief global MCS lock for testing */
+		argo::locallock::mcs_lock *mcs_lock;
+		/** @brief global cohort lock for testing */
+		argo::globallock::cohort_lock *cohort_lock;
+		/** @brief global tas lock type */
+		using tas_lock = argo::globallock::global_tas_lock;
+		/** @brief global tas lock for testing*/
+		tas_lock *global_tas_lock;
+		/** @brief field needed for the global tas lock*/
+		tas_lock::internal_field_type* field;
+		/** @brief global counter used in several tests */
+		int *counter;
 
-protected:
-	/** @brief local ticket lock for testing */
-	argo::locallock::ticket_lock *ticket_lock;
-	/** @brief global MCS lock for testing */
-	argo::locallock::mcs_lock *mcs_lock;
-	/** @brief global cohort lock for testing */
-	argo::globallock::cohort_lock *cohort_lock;
-	/** @brief global tas lock type */
-	using tas_lock = argo::globallock::global_tas_lock;
-	/** @brief global tas lock for testing*/
-	tas_lock *global_tas_lock;
-	/** @brief field needed for the global tas lock*/
-	tas_lock::internal_field_type* field;
-	/** @brief global counter used in several tests */
-	int *counter;
+		LockTest() {
+			argo::reset();
+			field = argo::conew_<tas_lock::internal_field_type>();
+			global_tas_lock = new tas_lock(field);
+			argo::barrier();
+		}
 
-	LockTest() {
-		argo::reset();
-		field = argo::conew_<tas_lock::internal_field_type>();
-		global_tas_lock = new tas_lock(field);
-		argo::barrier();
-	}
-
-	~LockTest() {
-		argo::codelete_(field);
-		delete global_tas_lock;
-		argo::barrier();
-	}
+		~LockTest() {
+			argo::codelete_(field);
+			delete global_tas_lock;
+			argo::barrier();
+		}
 };
+
 
 /**
  * @brief Tests trylock functionality
  */
-TEST_F(LockTest,TAS_trylock_all) {
+TEST_F(LockTest, TAS_trylock_all) {
 	bool *did_increment;
 	bool res;
 	counter = argo::conew_<int>();
 	did_increment = argo::conew_array<bool>(argo::number_of_nodes());
 
 	/* Init a counter and increment array to 0 */
-	if(argo::node_id() == 0){
+	if(argo::node_id() == 0) {
 		*counter = 0;
-		for (argo::num_nodes_t i = 0; i < argo::number_of_nodes(); i++){
+		for (argo::num_nodes_t i = 0; i < argo::number_of_nodes(); i++) {
 			did_increment[i] = false;
 		}
 	}
@@ -87,7 +89,7 @@ TEST_F(LockTest,TAS_trylock_all) {
 
 	/* All nodes try to take the lock and increment a shared counter */
 	ASSERT_NO_THROW(res = global_tas_lock->try_lock());
-	if(res){
+	if(res) {
 		(*counter)++;
 		did_increment[argo::node_id()] = true;
 		ASSERT_NO_THROW(global_tas_lock->unlock());
@@ -95,32 +97,31 @@ TEST_F(LockTest,TAS_trylock_all) {
 	argo::barrier();
 
 	/* At least not more than number_of_nodes nodes can have incremented the counter */
-	ASSERT_GE(argo::number_of_nodes(),*counter);
+	ASSERT_GE(argo::number_of_nodes(), *counter);
 
 	int sum = 0;
-	for (argo::num_nodes_t i = 0; i < argo::number_of_nodes(); i++){
+	for (argo::num_nodes_t i = 0; i < argo::number_of_nodes(); i++) {
 		if(did_increment[i])
 			sum++;
 	}
 
 	/* Compares counter value to flags set by each node */
-	ASSERT_EQ(sum,*counter);
+	ASSERT_EQ(sum, *counter);
 
 	argo::codelete_(counter);
 	argo::codelete_array(did_increment);
 }
 
-
 /** @brief Checks locking is working by implementing a custom barrier */
-TEST_F(LockTest,TAS_lock_custom_barrier) {
+TEST_F(LockTest, TAS_lock_custom_barrier) {
 	unsigned int tmp;
 
 	/* Checks this many times if barrier was successful */
-	long deadlock_threshold = 100000;
+	std::size_t deadlock_threshold = 100000;
 
 	counter = argo::conew_<int>();
 	/* Init a counter to 0 */
-	if(argo::node_id() == 0){
+	if(argo::node_id() == 0) {
 		*counter = 0;
 	}
 	argo::barrier();
@@ -131,18 +132,18 @@ TEST_F(LockTest,TAS_lock_custom_barrier) {
 	ASSERT_NO_THROW(global_tas_lock->unlock());
 
 	/* Poll for completion / all nodes incremented the counter */
-	do{
+	do {
 		ASSERT_NO_THROW(global_tas_lock->lock());
 		tmp = *counter;
 		ASSERT_NO_THROW(global_tas_lock->unlock());
 		/* Dont spin for too long */
-		if((--deadlock_threshold) == 0){
+		if((--deadlock_threshold) == 0) {
 			std::cout << "##### Risk for deadlock - exiting TAS_lock_custom_barrier test. #####" << std::endl;
 			return;
 		}
-	}while(tmp != argo::number_of_nodes());
+	} while (tmp != argo::number_of_nodes());
 
-	ASSERT_EQ(*counter,argo::number_of_nodes());
+	ASSERT_EQ(*counter, argo::number_of_nodes());
 	argo::codelete_(counter);
 }
 
@@ -231,7 +232,7 @@ TEST_F(LockTest, StressMCSMultipleLocks) {
 }
 
 /** @brief Checks locking is working by decrementing a shared counter */
-TEST_F(LockTest,StressCohortLock) {
+TEST_F(LockTest, StressCohortLock) {
 	std::thread threads[nThreads];
 	counter = argo::conew_<int>(0);
 	cohort_lock = new argo::globallock::cohort_lock();
@@ -251,6 +252,7 @@ TEST_F(LockTest,StressCohortLock) {
 	argo::codelete_(counter);
 	delete cohort_lock;
 }
+
 
 /**
  * @brief The main function that runs the tests
